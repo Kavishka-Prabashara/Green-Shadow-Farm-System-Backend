bd25597904b01cfdb9d7cb61ba38140697bf4e08
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2024-11-01 01:31:33 +0530

    initial commit

---
+++ src/main/java/com/green/shadow/greenshadowfarmsystembackend/GreenShadowFarmSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.green.shadow.greenshadowfarmsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowFarmSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowFarmSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/shadow/greenshadowfarmsystembackend/GreenShadowFarmSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.shadow.greenshadowfarmsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowFarmSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
